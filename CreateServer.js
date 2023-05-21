3ee8a7aae269481faa3deb0f6dc857a39d3317e6
karthik-140 <shanigaramkarthik@gmail.com>
2023-05-21 13:23:15 +0530

    install-nodemon-package

--- CreateServer.js
+++ CreateServer.js
@@ -1,8 +1,18 @@
 const http = require("http");
 
 const routes = require("./routes");
+const { inherits } = require("util");
 
 const server = http.createServer(routes.handler);
 console.log(routes.someText)
 
 server.listen(4000);
+
+
+
+/*commands to install nodemon-package for running the server automatically on save changes
+
+npm init   --> for creating the package.json file, select the options
+npm install nodemon --save-dev
+npm install 
+*/
